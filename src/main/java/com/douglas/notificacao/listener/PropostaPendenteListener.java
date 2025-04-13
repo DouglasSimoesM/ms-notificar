0bf58664538522297d1167fa32aa9dd9b59c27dc
DouglasSimoesM <douglas.simoesm@gmail.com>
2025-04-13 13:11:57 -0300

    Amazon SNS Config, RabbitMQ Config and test PROPOSTA_EM_ANALISE success

--- src/main/java/com/douglas/notificacao/listener/PropostaPendenteListener.java
+++ src/main/java/com/douglas/notificacao/listener/PropostaPendenteListener.java
@@ -4,17 +4,19 @@ import com.douglas.notificacao.constante.MensagemConstante;
 import com.douglas.notificacao.domain.Proposta;
 import com.douglas.notificacao.service.NotificacaoSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaPendenteListener {
 
+    @Autowired
     private NotificacaoSnsService notificacaoSnsService;
     //RabbitListener é um "ouvinte"
     @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
     public void PropostaPendente(Proposta proposta){
-        String mensage = String.format(MensagemConstante.PROPOSTA_EM_ANALISE,proposta.getUsuario().getNome());
-        notificacaoSnsService.notificar(mensage);
+        String mensage = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+        notificacaoSnsService.notificar(proposta.getUsuario().getTelefone(), mensage);
 
     }
 }
